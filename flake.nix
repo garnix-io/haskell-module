4fa67217982c00f4e78a61512518f19831fa2d9f
Sönke Hahn <soenkehahn@gmail.com>
2025-01-16 10:20:08 -0500

    Tweak option description

--- flake.nix
+++ flake.nix
@@ -39,7 +39,7 @@
 
       webServer = lib.mkOption {
         type = lib.types.nullOr (lib.types.submodule webServerSubmodule);
-        description = "If set, generates a NixOS configuration with an http server";
+        description = "Whether to create an HTTP server based on this Haskell project";
         default = null;
       };
 
